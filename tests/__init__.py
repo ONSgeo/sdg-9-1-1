3e84a77ef9afc50396a30b5a1435beba8e32a544
Ed Cuss <Ed.Cuss@ons.gov.uk>
2024-02-05 13:44:12 +0000

    minor test fixes

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,1 @@
-"""SDG 15.1.1 tests"""
-
-from typing import List
-from .sdg_base_tests import TestSDGBase
-
-__all__: List[str] = ["TestSDGBase"]
+"""SDG 9.1.1 tests"""
--- tests/test_sdg_9_1_1.py
+++ tests/test_sdg_9_1_1.py
@@ -1,8 +1,8 @@
 import pytest 
 from hypothesis import given, strategies as st
 
-from src.sdg_9_1_1_src.sdg_9_1_1 import SDG9_1_1
-from user_params import UserParams
+from src.sdg_9_1_1_src import SDG9_1_1
+from ..user_params import UserParams
 
 params = UserParams()
 
@@ -18,13 +18,17 @@ def test_check_similarity(arr1, arr2) -> None:
 
     assert isinstance(result, float)
     assert 0.0 <= result <= 100.0
+    assert result == len(set(arr1).intersection(set(arr2))) / len(set(arr1)) * 100
 
 
-@pytest.mark.parameterize("arr1, arr2, expected_res", [
-    ([], [], 100.0),
-    ([0, 1, 2], [0, 1, 2], 100.0),
-    ([0, 1, 2], [3, 4, 5], 0.0),
-])
+@pytest.mark.parameterize(
+    "arr1, arr2, expected_res", 
+    [
+        ([], [], 100.0),
+        ([0, 1, 2], [0, 1, 2], 100.0),
+        ([0, 1, 2], [3, 4, 5], 0.0),
+    ]
+)
 def test_check_similarity_defined_lists(arr1, arr2, expected_res) -> None:
     instance = SDG9_1_1("", params.root_dir)
 
@@ -33,3 +37,17 @@ def test_check_similarity_defined_lists(arr1, arr2, expected_res) -> None:
     assert actual_res == expected_res
 
 
+@given(
+    st.lists(st.integers(), min_size=1),
+    st.lists(st.integers(), min_size=1)
+)
+def test_get_min_len_array(arr1, arr2) -> None:
+    instance = SDG9_1_1("", params.root_dir)
+
+    result = instance.get_min_len_array(arr1, arr2)
+
+    assert isinstance(result, dict)
+    assert "min" in result
+    assert "max" in result
+    assert result["min"] == min(arr1, arr2, key=len)
+    assert result["max"] == max(arr1, arr2, key=len)
